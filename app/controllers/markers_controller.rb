4b2ddf8a536686202574fe9ba86571090d880c28
oleksandr <olexandr7@gmail.com>
2018-01-03 17:57:11 +0200

    seeds added

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,7 +1,30 @@
 class MarkersController < ApplicationController
+  before_action :set_marker, only: [:show]
+
+
+
   def index
+    respond_to do |format|
+  format.html
+  format.json {
+    pluck_fields = Marker.pluck(:id, :lat, :lng)
+    render json: Oj.dump(pluck_fields)
+  }
+end
   end
 
+
   def show
+        render "show", layout: false
   end
+
+
+
+  private
+    def set_marker
+      @marker = Marker.find(params[:id])
+    end
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Marker.create!(lat: 34.33, lng: 33.45, name: 'Hospital1',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
+Marker.create!(lat: 34.35, lng: 33.45, name: 'Hospital2',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
+Marker.create!(lat: 35.33, lng: 33.45, name: 'Hospital3',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
+Marker.create!(lat: 38.33, lng: 33.45, name: 'Hospital4',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
+Marker.create!(lat: 37.33, lng: 33.45, name: 'Hospital5',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
+Marker.create!(lat: 39.33, lng: 33.45, name: 'Hospital6',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
+Marker.create!(lat: 40.33, lng: 33.45, name: 'Hospital7',avatar: '',website: 'www.google.ba',adress: 'Sarajevo',about: 'Kosevo hospital');
