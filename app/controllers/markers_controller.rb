0468d8b69ac13d687da096329be76a3072a9a3b7
oleksandr <olexandr7@gmail.com>
2018-01-08 16:09:59 +0100

    auth refined

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,7 +1,7 @@
 class MarkersController < ApplicationController
   before_action :set_marker, only: [:show]
   #http_basic_authenticate_with name: "admin", password: "secret", only: [:new, :edit, :destroy]
-   http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+   http_basic_authenticate_with name: "admin", password: "secret", except: [:index, :all,:show]
 
   def index
     respond_to do |format|
